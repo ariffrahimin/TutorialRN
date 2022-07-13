5012b3cc2e01a03fdcdec7c4bb21baf2ebabad19
Ariff Rahimin <ariffrahimin99@gmail.com>
2022-07-13 15:00:22 +0800

    Counting

--- App.js
+++ App.js
@@ -1,29 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-
-
-const styles = StyleSheet.create({
-  center:{
-    alignItems:'center'
+class App extends Component{
+  state = {
+    count : 0
   }
-});
+onPress = () => {
+  this.setState({
+    count: this.state.count + 1
+  })
+}
 
-const Greeting = (props) => {
+render() {
   return (
-    <View style={styles.center}>
-      <Text>Hello {props.name}!</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+       style={styles.button}
+       onPress={this.onPress}
+      >
+       <Text>Click me</Text>
+      </TouchableOpacity>
+      <View>
+        <Text>
+          You clicked { this.state.count } times
+        </Text>
+      </View>
     </View>
   )
 }
-
-const LotsOfGreeting = () => {
-  return (
-    <View style={[styles.center, {top:50}]}>
-      <Greeting name ='Ariff'/>
-      <Greeting name ='Syamsul'/>
-      <Greeting name ='Mat Kilau'/>
-    </View>
-  );
 }
 
-export default LotsOfGreeting;
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+button: {
+  alignItems: 'center',
+  backgroundColor: '#DDDDDD',
+  padding: 10,
+  marginBottom: 10
+}
+})
+
+export default App;
+
